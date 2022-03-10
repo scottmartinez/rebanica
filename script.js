797a84fce94f4041522673e146d183af50a1ae65
Scott Martinez <scottmartinezolivas@hotmail.com>
2022-03-10 16:15:20 -0600

    agregados logos redes sociales

--- script.js
+++ script.js
@@ -5,14 +5,16 @@ const palabras = [{palabra: "abajarse", definicion: "Acción de bajarse" ,ejempl
                     {palabra: "acá", definicion: "lo mismo que aquí",ejemplo: "chavalo veni para acá"},
                     {palabra: "acalambrado", definicion: "tener miedo",ejemplo: "Ese mae te tiene acalambrado a vos"},
                     {palabra: " achantado", definicion: "triste",ejemplo: "Te tiene achantado la jaña mae"},
-                    {palabra: "afincar", definicion: "besar (de manera más intensa o de caracter sexual)",ejemplo: "Vos afincaste a la mae"}];
+                    {palabra: "afincar", definicion: "besar (de manera más intensa o de caracter sexual)",ejemplo: "Vos afincaste a la mae"},
+                    {palabra: "agarrar", definicion: "Tomar una cosa",ejemplo: "Vos agarraste la mano de la mae"},];
 
 const entrada_busqueda = document.getElementById("busqueda");
 entrada_busqueda.addEventListener("input", (e) => {
 let valor=e.target.value;
 if(valor&& valor.trim().length>0){
    valor=valor.trim().toLowerCase();
-    const resultados=palabras.filter(palabra => palabra.palabra.toLowerCase().includes(valor));
+   
+    const resultados=palabras.filter(palabra => palabra.palabra.toLowerCase().includes(valor)).slice(0,5);
    //console.log(resultados);
    renderResults(resultados);
 }
@@ -30,6 +32,16 @@ const renderResults = (results) => {
         <h3 class="titulo_palabra">${result.palabra}</h3>
         <p><strong> Definicion:</strong>${result.definicion}</p>
         <p><strong> Ejemplo:</strong>${result.ejemplo}</p>
+        <div id="area_compartir" class="area_compartir">
+        <a href="https://api.whatsapp.com/send?phone=&text=Oe%20prix%20mira%20este%20rebane%2C%20esta%20tuani" target="_blank">
+            <img src="assets/whatsapp-brands.svg" alt="Logo whatsapp" class="redes">
+        <a href="#facebook" target="_blank">
+            <img src="assets/facebook-brands.svg" alt="Logo Facebook" class="redes">
+        <a href="#instagram" target="_blank">
+            <img src="assets/instagram-brands.svg" alt="Logo Instagram" class="redes">
+        <a href="#twitetr" target="_blank">
+            <img src="assets/twitter-brands.svg" alt="Logo Twitter" class="redes">
+        </div>
     </li>
     `).join('');
     document.getElementById('resultados').innerHTML = html;
